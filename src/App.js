ba1ecfb179a2020a61d5d03023554e7baf76360d
Max Mohammadi <max.mohammadi@sydereal.com>
2021-01-15 14:36:30 -0800

    Add users functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios';
+import DemoAPI from './DemoAPI'
 
 class App extends Component {
     state = {
@@ -18,9 +20,33 @@ class App extends Component {
     }
 
     handleSubmit = character => {
-        this.setState({ characters: [...this.state.characters, character] })
+        this.makePostCall(character).then( callResult => {
+            if (callResult === true) {
+                this.setState({ characters: [...this.state.characters, character] });
+            }
+        });
+    }
+    
+    makePostCall(character) {
+        return axios.post('http://localhost:5000/users', character).then(function (response) {
+            console.log(response);
+            return (response.status === 201);
+        }).catch(function (error) {
+            console.log(error);
+            return false;
+        });
     }
 
+    componentDidMount() {
+        const url = 'http://localhost:5000/users'
+        axios.get(url).then(res => {
+            const characters = res.data.users_list;
+            this.setState({ characters });
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+    
     render() {
         const { characters } = this.state
         
@@ -28,8 +54,9 @@ class App extends Component {
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter} />
                 <Form handleSubmit={this.handleSubmit} />
+                {/* <FetchDemo subreddit='r/calpoly'/> */}
             </div>
-        )
+        );
     }
 }
 
